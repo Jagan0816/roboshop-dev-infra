b6917074d3d43a8923e7bf08bb2c0a7dcdcaaa4a
Jagan0816 <jagandevops9@gmail.com>
2026-07-04 22:10:07 +0530

    terraform

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z06893813KBQOA69VT6MI"   
+}
+
+variable "domain_name" {
+    default = "jagandevops.online"
+}

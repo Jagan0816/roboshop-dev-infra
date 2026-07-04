d4bf8be398cb5ed9d845b3d0ed4d7c4f296bef31
Jagan0816 <jagandevops9@gmail.com>
2026-07-04 15:39:30 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -71,7 +71,7 @@ resource "terraform_data" "redis" {
     provisioner "remote-exec" {
       inline = [ 
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
+        "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
       ]
     }
 }
